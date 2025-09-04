7841c07314a7ce1a5d435560f0d1ad8255e87f3c
kmoreno <morenokarenanahi24@gmail.com>
2025-09-04 00:12:29 -0300

    Sobreescritura de metodos

---
+++ 3.POO/Herencia3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gato g1 = new Gato();
+            g1.Nombre = "Luna"; 
+            Perro p1 = new Perro();
+            p1.Nombre = "Eddie";
+
+            Console.WriteLine(g1.comunicarse());
+            Console.WriteLine(p1.comunicarse());
+            Console.ReadKey();
+        }
+    }
+}
