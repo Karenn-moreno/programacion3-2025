aeaee5db509777ac8c1a275ca1b1c37e40e6a057
kmoreno <morenokarenanahi24@gmail.com>
2025-09-05 15:07:07 -0300

    introduccion Winform

--- 3.POO/Herencia/Persona.cs
+++ 3.POO/Herencia/Persona.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Herencia
 {
-   abstract internal class Persona
+   sealed internal class Persona
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
