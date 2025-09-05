b67ec503d4e1f616d44ccc8a6db52914f0d9a36f
kmoreno <morenokarenanahi24@gmail.com>
2025-09-05 00:25:11 -0300

    interfases

--- 3.POO/Herencia3/Program.cs
+++ 3.POO/Herencia3/Program.cs
@@ -15,7 +15,7 @@ namespace Herencia3
             Perro p1 = new Perro();
             p1.Nombre = "Eddie";
 
-            List<Animal>  animales= new List<Animal>(); 
+            List<Animal> animales = new List<Animal>();
             animales.Add(p1);
             animales.Add(new Pez());
             animales.Add(new Canario());
@@ -23,15 +23,21 @@ namespace Herencia3
             animales.Add(new Aguila());
             animales.Add(new Gato());
 
-           
-            foreach (Animal item in animales) //recorrer la lista
-            {
-                Console.WriteLine(item.comunicarse());
-            }
+            //interfases
+            List <Flyable> listaVoladores =new List<Flyable>();
+            listaVoladores.Add(new Canario());
+            listaVoladores.Add(new Aguila());
+
+            //foreach (Animal item in animales) //recorrer la lista
+            //{
+            //    Console.WriteLine(item.comunicarse());
+            //}
+
+
             /*POLIMORFISMO*/
 
             /* Animal a1 = g1;
-             Gato g8=(Gato)a1; //casteo 
+             Gato g8=(Gato)a1; //  Casteo 
              g8.Nombre = "Morenita";
             */
 
