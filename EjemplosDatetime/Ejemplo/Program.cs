6af84ef9b94db654257662dd4ae7636c756490b2
kmoreno <morenokarenanahi24@gmail.com>
2025-09-07 00:34:48 -0300

    Tipo DateTime

---
+++ EjemplosDatetime/Ejemplo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha = DateTime.Now;
+
+            //fecha.Year asi para los diferentes datos trabajar por separado..
+            Console.Write("La fecha es :"+ fecha.ToString("dd/MM/yy"));
+            Console.ReadKey();
+        }
+    }
+}
+     
---
+++ EjemplosDatetime/Ejemplo2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrueba1_Click(object sender, EventArgs e)
+        {
+            //capturar el valor 
+            DateTime fecha1;
+
+            fecha1 = dtpFecha.Value; //le asigno a la variable un valor tipo fecha
+
+            //trabajo con la variable
+            MessageBox.Show("La fecha seleccionada es : " + fecha1.ToString("dd/MM/yy"));
+        }
+
+        private void btnPrueba2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La fecha seleccionada es : " + calFecha.SelectionStart.ToString("dd/MM/yy"));
+        }
+    }
+}
